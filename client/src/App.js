04bc5c5dc859d565a3d8c454f53dd309eb09511d
sanjayprasadF5 <sanjayprasad2321998@gmail.com>
2021-12-01 13:04:29 +0530

    started with ui part material

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,45 @@
 import logo from "./logo.svg";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import "./App.css";
-
+import Student from "./components/showStudent/showStudent.js";
+import Create from "./components/createStudent/createStudent.js";
+import useStyles from "./styles";
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello </h1>
-      </header>
+      <Container maxWidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
+            Student Create and Show
+          </Typography>
+        </AppBar>
+
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch">
+              <Grid item xs={12} sm={7}>
+                <AppBar
+                  className={classes.appBar}
+                  position="static"
+                  color="inherit"
+                >
+                  <Student />
+                </AppBar>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <AppBar
+                  className={classes.appBar}
+                  position="static"
+                  color="inherit"
+                >
+                  <Create />
+                </AppBar>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
